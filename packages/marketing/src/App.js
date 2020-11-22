ff4dfee898f983dac381f4f8efc829d1515ffb94
Diogo Costa <dsgmcosta@gmail.com>
2020-11-22 19:22:45 +0000

    linter added, some work on webpack prod configs

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -5,15 +5,17 @@ import { StylesProvider } from "@material-ui/core/styles"
 import Landing from "./components/Landing"
 import Pricing from "./components/Pricing"
 
-export default () => (
-  <div>
-    <StylesProvider>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/pricing" component={Pricing} />
-          <Route path="/" component={Landing} />
-        </Switch>
-      </BrowserRouter>
-    </StylesProvider>
-  </div>
-)
+export default function App() {
+  return (
+    <div>
+      <StylesProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </BrowserRouter>
+      </StylesProvider>
+    </div>
+  )
+}
